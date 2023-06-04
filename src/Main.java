7db117b758b0035e8dd0d648b1b6143f6c0cec6f
annikaferguson <fergusak@mail.uc.edu>
2023-06-04 13:14:05 -0400

    lab 04 q4

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+     int numToExamine = 2;
+     int moduloNum = 2;
+
+     moduloNum = numToExamine / 2;
+
+     if (moduloNum == 0);
+        {
+            System.out.println("The number is even!");
+        }
+    }
+}
